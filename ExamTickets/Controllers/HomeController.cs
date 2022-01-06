b4d25db5cdafa4b990b29a3f6fe0559272fb1039
kanadeiar <kanadeiar@gmail.com>
2022-01-06 05:33:00 +0300

    base app infrastructure

---
+++ ExamTickets/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamTickets.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    public IActionResult Error(string id)
+    {
+        switch (id)
+        {
+            default: return Content($"Status --- {id}");
+            case "404": return View("Error404");
+        }
+    }
+}
